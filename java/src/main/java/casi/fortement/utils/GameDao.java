6febf255f0b461641c67dd961282581f934b66a6
pierre-sassoulas <pierre.sassoulas@gmail.com>
2014-01-09 20:55:07 +0100

    qazsdcvbuij

--- java/src/main/java/casi/fortement/utils/GameDao.java
+++ java/src/main/java/casi/fortement/utils/GameDao.java
@@ -1,66 +1,41 @@
 package casi.fortement.utils;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
-import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import casi.fortement.pojo.JeuSteam;
 
+@Component
 public class GameDao {
 
 	@Autowired
-	private DataSource dataSource;
+	private JdbcTemplate jdbcTemplate;
 
-	public void insert(JeuSteam jeuSteam) {
-
-		String sql = "INSERT INTO users "
-				+ "(id_user, game_id, playtime) VALUES (?, ?, ?)";
-		Connection conn = null;
-
-		try {
-			conn = dataSource.getConnection();
-			PreparedStatement ps = conn.prepareStatement(sql);
-			ps.setInt(2, jeuSteam.getGameId());
-			ps.setInt(3, jeuSteam.getPlaytimeForever());
-			ps.executeUpdate();
-			ps.close();
-
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-
-		} finally {
-			if (conn != null) {
-				try {
-					conn.close();
-				} catch (SQLException e) {
-				}
-			}
-		}
-	}
-
+	@SuppressWarnings("unchecked")
 	public List<JeuSteam> recupererJeux(String user_id) {
-		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-		Collection jeux = jdbcTemplate.query(
-				"select first_name, surname from t_actor", new RowMapper() {
-
+		Collection jeux = this.jdbcTemplate.query(
+				"select id_game, from t_actor", new RowMapper() {
 					public Object mapRow(ResultSet rs, int rowNum)
 							throws SQLException {
-						Actor actor = new Actor();
-						actor.setFirstName(rs.getString("first_name"));
-						actor.setSurname(rs.getString("surname"));
-						return actor;
+						JeuSteam jeu = new JeuSteam();
+						jeu.setGameId(rs.getInt("id_game"));
+						jeu.setPlaytimeForever(rs.getInt("playtime"));
+						return jeu;
 					}
 				});
-		return jeux;
+		List<JeuSteam> resultats = new ArrayList<JeuSteam>();
+		for (Object jeu : jeux) {
+			resultats.add((JeuSteam) jeu);
+		}
+		return resultats;
 	}
 
 }
---
+++ java/src/main/webapp/WEB-INF/views/resultats.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Page de ${user_name}</title>
+
+</head>
+<body>
+
+	<h1>Jeux possédés</h1>
+	<c:forEach items="${jeux }" var="jeu">
+		${jeu.gameId} <br />
+	</c:forEach>
+
+	<h1>Jeux suggérés</h1>
+
+	<c:forEach items="${jeuxSuggeres }" var="jeu">
+		${jeu.nom} <br />
+	</c:forEach>
+</body>
+</html>
