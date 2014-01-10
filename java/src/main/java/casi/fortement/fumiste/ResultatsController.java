5381bb96eebb984825dec7f666f4f97a46ec4497
fxmatthews <fxmatthews@gmail.com>
2014-01-10 06:44:45 +0100

    recommandation fonctionnelle

--- java/src/main/java/casi/fortement/fumiste/ResultatsController.java
+++ java/src/main/java/casi/fortement/fumiste/ResultatsController.java
@@ -14,7 +14,6 @@ import casi.fortement.utils.GameDao;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -33,153 +32,161 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 @Controller
-    public class ResultatsController {
+public class ResultatsController {
 
-    @Autowired
+	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
-    @RequestMapping("resultats/{id}")
+	@RequestMapping("resultats/{id}")
 	public ModelAndView aficherResulats(@PathVariable String id) {
-	ModelAndView result = new ModelAndView("resultats");
+		ModelAndView result = new ModelAndView("resultats");
 
-	List<JeuSteam> jeux = recupererJeux(id);
+		List<JeuSteam> jeux = recupererJeux(id);
 
+		result.addObject("jeux", getGamesNames(jeux));
+		result.addObject("recommandations", getRecommandations(id));
 
-	result.addObject("jeux", getGamesNames(jeux));
-	result.addObject("recommandations", getRecommandations(id));
-
-	return result;
-    }
-
-    private List<JeuSteam> recupererJeux(String user_id) {
-	Collection jeux = this.jdbcTemplate.query(
-						  "select id_game, playtime from users where id_user=" + user_id, new RowMapper() {
-							  public Object mapRow(ResultSet rs, int rowNum)
-							      throws SQLException {
-							      JeuSteam jeu = new JeuSteam();
-							      jeu.setGameId(rs.getInt("id_game"));
-							      jeu.setPlaytimeForever(rs.getInt("playtime"));
-							      return jeu;
-							  }
-						      });
-	List<JeuSteam> resultats = new ArrayList<JeuSteam>();
-	for (Object jeu : jeux) {
-	    resultats.add((JeuSteam) jeu);
-	}
-	return resultats;
-    }
-
-    private List<String> getRecommandations(String userId){
-	Collection jeux = this.jdbcTemplate.query(
-						  "select * from users where id_user <>" + user_id, new RowMapper() {
-							  public Object mapRow(ResultSet rs, int rowNum)
-							      throws SQLException {
-							      JeuSteam jeu = new JeuSteam();
-							      jeu.setProprioId("id_user");
-							      jeu.setGameId(rs.getInt("id_game"));
-							      jeu.setPlaytimeForever(rs.getInt("playtime"));
-							      return jeu;
-							  }
-						      });
-	// Recuperation de la liste de jeu de chaque joueur
-	Map<String, List<JeuSteam>> tmp = new HashMap<String, List<JeuSteam>>();
-	Map<String, Integer> compteurJeux = new HashMap<String, Integer>();
-	for (Object jeu : jeux) {
-	    JeuSteam jeuTmp = (JeuSteam) jeu;
-	    List<JeuSteam> listTmp = tmp.get(jeu.getProprioId());
-	    listTmp.add(jeuTmp);
-	    tmp.put(jeu.getProprioId(), listTmp);
-	    compteurJeux.put(jeu.getGameId(), 0);
+		return result;
 	}
 
-
-	//  on lance le calcul
-	for (List<JeuSteam> collectionUser : tmp.values()){
-	    List<JeuSteam> jeuxPreferesTmp = new ArrayList<JeuSteam>();
-	    int compteur = 0;
-	    List<Integer> tempsTmp = new ArrayList<Integer>();
-	    for(JeuSteam jeu : collectionUser){
-		if(compteur <3){
-		    jeuxPreferesTmp.add(jeu);
-		    tempsTmp.add(jeu.getPlayTimeForever());
-		    compteur ++;
-		} else {
-		    if(jeu.getPlayTimeForever() > Collections.min(tempsTmp)){
-			jeuxPreferesTmp.remove(jeuxPreferesTmp.lastIndexOf(Collections.min(tempsTmp)));
-			tempsTmp.remove(tempsTmp.lastIndexOf(Collections.min(tempsTmp)));
-			jeuxPreferesTmp.add(jeu);
-			tempsTmp.add(jeu.getPlayTimeForever());
-		    }
+	private List<JeuSteam> recupererJeux(String user_id) {
+		Collection jeux = this.jdbcTemplate.query(
+				"select id_game, playtime from users where id_user=" + user_id,
+				new RowMapper() {
+					public Object mapRow(ResultSet rs, int rowNum)
+							throws SQLException {
+						JeuSteam jeu = new JeuSteam();
+						jeu.setGameId(rs.getInt("id_game"));
+						jeu.setPlaytimeForever(rs.getInt("playtime"));
+						return jeu;
+					}
+				});
+		List<JeuSteam> resultats = new ArrayList<JeuSteam>();
+		for (Object jeu : jeux) {
+			resultats.add((JeuSteam) jeu);
 		}
-	    }
-	    for(JeuSteam jeu : jeuxPreferesTmp){
-		int idTmp = jeu.getGameId();
-		compteurJeux.put(String.valueOf(idTmp), String.valueOf(idTmp + 1));
-	    }
-	    
+		return resultats;
 	}
-	compteurJeux = sortHashMapByValuesD(compteurJeux);
 
-	List<String> result = new ArrayList<String>();
-	Iterator it = compteurJeux.entrySet().iterator();
-	int compteur = 0;
-	while (it.hasNext() && compteur < 3) {
-	    Map.Entry pairs = (Map.Entry)it.next();
-	    result.add(getGameName(pairs.getKey()));
-	    compteur ++;
-	    it.remove(); // avoids a ConcurrentModificationException
+	private List<String> getRecommandations(String userId) {
+		Collection jeux = this.jdbcTemplate.query(
+				"select * from users where id_user <>" + userId,
+				new RowMapper() {
+					public Object mapRow(ResultSet rs, int rowNum)
+							throws SQLException {
+						JeuSteam jeu = new JeuSteam();
+						jeu.setProprioId("id_user");
+						jeu.setGameId(rs.getInt("id_game"));
+						jeu.setPlaytimeForever(rs.getInt("playtime"));
+						return jeu;
+					}
+				});
+		// Recuperation de la liste de jeu de chaque joueur
+		Map<String, List<JeuSteam>> tmp = new HashMap<String, List<JeuSteam>>();
+		HashMap<String, Integer> compteurJeux = new HashMap<String, Integer>();
+
+		for (Object jeu : jeux) {
+			JeuSteam jeuTmp = (JeuSteam) jeu;
+			List<JeuSteam> listTmp = new ArrayList<JeuSteam>();
+			if (tmp.containsKey(jeuTmp.getProprioId()))
+				listTmp = tmp.get(jeuTmp.getProprioId());
+			listTmp.add(jeuTmp);
+			tmp.put(jeuTmp.getProprioId(), listTmp);
+			compteurJeux.put(String.valueOf(jeuTmp.getGameId()), 0);
+		}
+
+		// on lance le calcul
+		for (List<JeuSteam> collectionUser : tmp.values()) {
+			List<JeuSteam> jeuxPreferesTmp = new ArrayList<JeuSteam>();
+			int compteur = 0;
+			List<Integer> tempsTmp = new ArrayList<Integer>();
+			for (JeuSteam jeu : collectionUser) {
+				if (compteur < 3) {
+					jeuxPreferesTmp.add(jeu);
+					tempsTmp.add(jeu.getPlaytimeForever());
+					compteur++;
+				} else {
+					if (jeu.getPlaytimeForever() > Collections.min(tempsTmp)) {
+						jeuxPreferesTmp.remove(tempsTmp
+								.lastIndexOf(Collections.min(tempsTmp)));
+						tempsTmp.remove(tempsTmp.lastIndexOf(Collections
+								.min(tempsTmp)));
+						jeuxPreferesTmp.add(jeu);
+						tempsTmp.add(jeu.getPlaytimeForever());
+					}
+				}
+			}
+			for (JeuSteam jeu : jeuxPreferesTmp) {
+				int idTmp = jeu.getGameId();
+				compteurJeux.put(String.valueOf(idTmp),
+						compteurJeux.get(String.valueOf(idTmp)) + 1);
+			}
+
+		}
+		compteurJeux = sortHashMapByValuesD(compteurJeux);
+
+		List<String> result = new ArrayList<String>();
+		Iterator it = compteurJeux.entrySet().iterator();
+		int compteur = 0;
+		while (it.hasNext() && compteur < 3) {
+			Map.Entry pairs = (Map.Entry) it.next();
+			result.add(getGameName((String) pairs.getKey()));
+			compteur++;
+			it.remove(); // avoids a ConcurrentModificationException
+		}
+		return result;
 	}
-	return result;
-    }
 
-    private List<String> getGamesNames(List<JeuSteam> gamesId){
-	List<String> nomsJeux = new ArrayList<String>();
-	System.out.println("On chope les noms des jeux, veuillez patienter.");
-	for (JeuSteam jeu : gamesId){
-	    nomsJeux.add(String.valueOf(jeu.getGameId()));
+	private List<String> getGamesNames(List<JeuSteam> gamesId) {
+		List<String> nomsJeux = new ArrayList<String>();
+		System.out.println("On chope les noms des jeux, veuillez patienter.");
+		for (JeuSteam jeu : gamesId) {
+			nomsJeux.add(getGameName(String.valueOf(jeu.getGameId())));
+		}
+		return nomsJeux;
 	}
-	return nomsJeux;
-    }
 
-    private String getGameName(String gameId){
-	String result = "";
-	    try{
-		Document doc = Jsoup.connect("http://store.steampowered.com/app/" + gameId).get();
-		result =  (doc.title().replace("on Steam", ""));
-	    }catch (java.io.IOException e){
-		e.printStackTrace();
-	    }
-	    return result;
-    }
+	private String getGameName(String gameId) {
+		String result = "";
+		try {
+			Document doc = Jsoup.connect(
+					"http://store.steampowered.com/app/" + gameId).get();
+			result = (doc.title().replace("on Steam", ""));
+		} catch (java.io.IOException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
 
-private LinkedHashMap sortHashMapByValuesD(HashMap passedMap) {
-   List mapKeys = new ArrayList(passedMap.keySet());
-   List mapValues = new ArrayList(passedMap.values());
-   Collections.sort(mapValues);
-   Collections.sort(mapKeys);
+	private LinkedHashMap<String, Integer> sortHashMapByValuesD(
+			HashMap<String, Integer> passedMap) {
+		List<String> mapKeys = new ArrayList(passedMap.keySet());
+		List<Integer> mapValues = new ArrayList(passedMap.values());
+		Collections.sort(mapValues);
+		Collections.sort(mapKeys);
 
-   LinkedHashMap sortedMap = new LinkedHashMap();
+		LinkedHashMap<String, Integer> sortedMap = new LinkedHashMap();
 
-   Iterator valueIt = mapValues.iterator();
-   while (valueIt.hasNext()) {
-       Object val = valueIt.next();
-       Iterator keyIt = mapKeys.iterator();
+		Iterator valueIt = mapValues.iterator();
+		while (valueIt.hasNext()) {
+			Object val = valueIt.next();
+			Iterator keyIt = mapKeys.iterator();
 
-       while (keyIt.hasNext()) {
-           Object key = keyIt.next();
-           String comp1 = passedMap.get(key).toString();
-           String comp2 = val.toString();
+			while (keyIt.hasNext()) {
+				Object key = keyIt.next();
+				String comp1 = passedMap.get(key).toString();
+				String comp2 = val.toString();
 
-           if (comp1.equals(comp2)){
-               passedMap.remove(key);
-               mapKeys.remove(key);
-               sortedMap.put((String)key, (Double)val);
-               break;
-           }
+				if (comp1.equals(comp2)) {
+					passedMap.remove(key);
+					mapKeys.remove(key);
+					sortedMap.put((String) key, (Integer) val);
+					break;
+				}
 
-       }
+			}
 
-   }
-   return sortedMap;
-}
+		}
+		return sortedMap;
+	}
 }
